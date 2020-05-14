78bb57588e449b01c934228fa64932c241214ea9
mbshbn <mbshbn@gwmail.gwu.edu>
2020-05-14 12:02:50 -0400

    Type: feat
    draw line on th final image

--- output_images/Line_detection_advanced.py
+++ output_images/Line_detection_advanced.py
@@ -4,29 +4,6 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 
-data = np.load('calib_info.npz')
-mtx = data['mtx']
-dist = data['dist']
-
-# Undistord a road image:
-# Read in the distorted test image
-image = cv2.imread('../test_images/straight_lines1.jpg')
-# Undistorting the test image:
-undist = cv2.undistort(image, mtx, dist, None, mtx)
-"""
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
-f.tight_layout()
-ax1.imshow(img)
-ax1.set_title('Original Image', fontsize=50)
-ax2.imshow(undist)
-ax2.set_title('Undistorted Image', fontsize=50)
-plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-plt.show()
-mpimg.imsave("road_undistorted.png", undist)
-"""
-# Note: img is the undistorted image
-img = np.copy(undist)
-
 def grad_thresh(img, thresh=(20,100)):
     # Gradient thresholds:
     # Grayscale image
@@ -52,22 +29,18 @@ def color_thresh(img, thresh=(170,255)):
     s_binary[(s_channel >= thresh[0]) & (s_channel <= thresh[1])] = 1
     return s_binary
 
-def warp(img, src, dst ):
+def warp(img, src, dst, img_size):
 
     #compute the perspective trasform, matrix M
     M = cv2.getPerspectiveTransform(src, dst)
 
-    # Could compute the inverse by swapping the input parameters
-    # minv = cv2.getPerspectiveTransform(dst, src)
-
     # creat warped image -uses linear interpolation
     warped = cv2.warpPerspective(img, M, img_size,flags=cv2.INTER_LINEAR)
-    plt.imshow(warped)
-    plt.show()
 
     return warped
 
 def find_lane_pixels(binary_warped):
+
     # Take a histogram of the bottom half of the image
     histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
     # Create an output image to draw on and visualize the result
@@ -148,15 +121,14 @@ def find_lane_pixels(binary_warped):
 
     return leftx, lefty, rightx, righty, out_img
 
-
-def fit_polynomial(out_img, leftx, lefty, rightx, righty):
+def fit_polynomial(out_img, leftx, lefty, rightx, righty, size_binary_warped_0):
 
     # Fit a second order polynomial to each using `np.polyfit`
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
 
     # Generate x and y values for plotting
-    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    ploty = np.linspace(0, size_binary_warped_0-1, size_binary_warped_0 )
     try:
         left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
         right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
@@ -172,106 +144,262 @@ def fit_polynomial(out_img, leftx, lefty, rightx, righty):
     out_img[righty, rightx] = [0, 0, 255]
 
     # Plots the left and right polynomials on the lane lines
-    plt.plot(left_fitx, ploty, color='yellow')
-    plt.plot(right_fitx, ploty, color='yellow')
+    # plt.plot(left_fitx, ploty, color='yellow')
+    # plt.plot(right_fitx, ploty, color='yellow')
 
-    return out_img
+    color=[255, 0, 0]
+    thickness=2
+    cv2.line(out_img, (left_fitx[0].astype(int), ploty[0].astype(int)),\
+     (left_fitx[-1].astype(int), ploty[-1].astype(int)), color, thickness)
+    cv2.line(out_img, (right_fitx[0].astype(int), ploty[0].astype(int)),\
+     (right_fitx[-1].astype(int), ploty[-1].astype(int)), color, thickness)
 
-sx_binary = grad_thresh(img, thresh=(20,100))
-s_binary = color_thresh(img, thresh=(170,255))
-# Stack each channel to view their individual contributions in green and blue respectively
-# This returns a stack of the two binary images, whose components you can see as different colors
-color_binary = np.dstack(( np.zeros_like(sx_binary), sx_binary, s_binary)) * 255
+    return out_img, ploty, left_fitx, right_fitx
 
-# Combine the two binary thresholds
-combined_binary = np.zeros_like(sx_binary)
-combined_binary[(s_binary == 1) | (sx_binary == 1)] = 1
+def measure_curvature_real(ploty, left_fit_cr, right_fit_cr, ym_per_pix, xm_per_pix):
+    '''
+    Calculates the curvature of polynomial functions in meters.
+    '''
+    # Define y-value where we want radius of curvature
+    # We'll choose the maximum y-value, corresponding to the bottom of the image
+    y_eval = np.max(ploty)
 
-# Define calibration box in source (original) and destination
-# (desired, warped coordinates)
-img_size = (img.shape[1], img.shape[0])
+    # Calculation of R_curve (radius of curvature)
+    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
 
-# 4 source image points
-src = np.float32(
-[[(img_size[0] / 2) - 55, img_size[1] / 2 + 100],#top left
-[((img_size[0] / 6) - 10), img_size[1]],#bottomleft
-[(img_size[0] * 5 / 6) + 60, img_size[1]],# bottom right
-[(img_size[0] / 2 + 55), img_size[1] / 2 + 100]])# top right
+    return left_curverad, right_curverad
 
-# 4 desired coordinates
-dst = np.float32(
-[[(img_size[0] / 4), 0],
-[(img_size[0] / 4), img_size[1]],
-[(img_size[0] * 3 / 4), img_size[1]],
-[(img_size[0] * 3 / 4), 0]])
+def unwarp(img, src, dst, img_size):
 
-# get perspective transform of the binary image
-binary_warped = warp(combined_binary, src, dst)
+    # Could compute the inverse by swapping the input parameters
+    Minv = cv2.getPerspectiveTransform(dst, src)
+
+    # creat warped image -uses linear interpolation
+    unwarped = cv2.warpPerspective(img, Minv, img_size,flags=cv2.INTER_LINEAR)
+
+    return unwarped
+
+def Lane_Finding_Pipeline_Image_Advanced(image_road):
+
+    data = np.load('calib_info.npz')
+    mtx = data['mtx']
+    dist = data['dist']
+    #TODO: write the matrices here
+
+    # undist_roadorting the test image_road:
+    undist_road = cv2.undistort(image_road, mtx, dist, None, mtx)
+
+    # f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+    # f.tight_layout()
+    # ax1.imshow(image_road)
+    # ax1.set_title('Original Image', fontsize=10)
+    # ax2.imshow(undist_road)
+    # ax2.set_title('Undistorted Image', fontsize=10)
+    # plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+    # mpimg.imsave("road_undistorted.png", undist_road)
+    # plt.show()
+
+    # Note: img is the undistorted image
+    img = np.copy(undist_road)
+
+    sx_binary = grad_thresh(img, thresh=(20,100))
+    s_binary = color_thresh(img, thresh=(170,255))
+    # Stack each channel to view their individual contributions in green and blue respectively
+    # This returns a stack of the two binary images, whose components you can see as different colors
+    # color_binary = np.dstack(( np.zeros_like(sx_binary), sx_binary, s_binary)) * 255
+
+    # Combine the two binary thresholds
+    combined_binary = np.zeros_like(sx_binary)
+    combined_binary[(s_binary == 1) | (sx_binary == 1)] = 1
+
+    # f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(24, 9))
+    # f.tight_layout()
+    # ax1.imshow(sx_binary)
+    # ax1.set_title('grad thresh binary (sobel x)', fontsize=10)
+    # ax2.imshow(s_binary)
+    # ax2.set_title('color thresh binary (S from HSV)', fontsize=10)
+    # ax3.imshow(combined_binary)
+    # ax3.set_title('grad & color combined', fontsize=10)
+    # plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+    # mpimg.imsave("thresh_cmbined_binary.png", combined_binary)
+    # plt.show()
+
+    # Define calibration box in source (original) and destination
+    # (desired, warped coordinates)
+    img_size = (img.shape[1], img.shape[0])
+
+    # 4 source image points
+    src = np.float32(
+    [[(img_size[0] / 2) - 60, img_size[1] / 2 + 100],#top left
+    [((img_size[0] / 6) - 10), img_size[1]],#bottomleft
+    [(img_size[0] * 5 / 6) + 45, img_size[1]],# bottom right
+    [(img_size[0] / 2 + 60), img_size[1] / 2 + 100]])# top right
+
+    # 4 desired coordinates
+    dst = np.float32(
+    [[(img_size[0] / 4), 0],
+    [(img_size[0] / 4), img_size[1]],
+    [(img_size[0] * 3 / 4), img_size[1]],
+    [(img_size[0] * 3 / 4), 0]])
+
+    # get perspective transform of the binary image
+    binary_warped = warp(combined_binary, src, dst, img_size)
+    # plt.imshow(binary_warped)
+    # plt.title('binary warped', fontsize=10)
+    # mpimg.imsave("binary_warped.png", binary_warped)
+    # plt.show()
+
+    # For fun: get perspective transform of the original road image
+    # plt.plot(src[0,0],src[0,1],'.')
+    # plt.plot(src[1,0],src[1,1],'.')
+    # plt.plot(src[2,0],src[2,1],'.')
+    #plt.plot(src[3,0],src[3,1],'.')
+    #plt.plot(dst[0,0],dst[0,1],'.')
+    #plt.plot(dst[1,0],dst[1,1],'.')
+    #plt.plot(dst[2,0],dst[2,1],'.')
+    #plt.plot(dst[3,0],dst[3,1],'.')
+    # road_rectangale=cv2.line(undist_road, (src[0,0],src[0,1]), (src[1,0],src[1,1]), (0, 255, 0) , 2)
+    # road_rectangale=cv2.line(road_rectangale, (src[3,0],src[3,1]), (src[2,0],src[2,1]), (0, 255, 0) , 2)
+    # road_rectangale=cv2.line(road_rectangale, (src[0,0],src[0,1]), (src[3,0],src[3,1]), (0, 255, 0) , 2)
+    # road_rectangale=cv2.line(road_rectangale, (src[1,0],src[1,1]), (src[2,0],src[2,1]), (0, 255, 0) , 2)
+    # plt.imshow(road_rectangale)
+    # plt.title('road with rectangle', fontsize=10)
+    # mpimg.imsave("image_rectangale.png", road_rectangale)
+    # plt.show()
+
+    # warped_road = warp(road_rectangale, src, dst, img_size)
+    # plt.imshow(warped_road)
+    # plt.title('warped road with rectangle', fontsize=10)
+    # mpimg.imsave("warped_image_rectangale.png", warped_road)
+    # plt.show()
+
+    # Find our lane pixels first
+    leftx, lefty, rightx, righty, binary_warped_pixel = find_lane_pixels(binary_warped)
+    # plt.imshow(binary_warped_pixel)
+    # plt.title('binary warped pixel', fontsize=10)
+    # mpimg.imsave("binary_warped_pixel.png", binary_warped_pixel)
+    # plt.show()
+
+    # Fit a polynomial
+    binary_warped_line, ploty, left_fit_cr, right_fit_cr = fit_polynomial(binary_warped_pixel, leftx, lefty, rightx, righty, binary_warped.shape[0])
+    # plt.imshow(binary_warped_line)
+    # plt.title('binary warped line', fontsize=10)
+    # plt.show()
+    #mpimg.imsave("binary_warped_line.png", binary_warped_line)
+
+    # print(np.shape(left_fit_cr))#, right_fit_cr
+    # print(left_fit_cr[0])
+    # print(left_fit_cr[-1])
+
+    uuwarped_binary = unwarp(binary_warped_line, src, dst, img_size)
+    # plt.imshow(uuwarped_binary)
+    # plt.title('unwarped binary', fontsize=10)
+    # mpimg.imsave("uuwarped_binary.png", uuwarped_binary)
+    # plt.show()
+
+    # Fit a polynomial
+    # warped_road_line, ploty, left_fit_cr, right_fit_cr = fit_polynomial(warped_road, leftx, lefty, rightx, righty, binary_warped.shape[0])
+    # plt.imshow(warped_road_line)
+    # plt.title('warped road line 1', fontsize=10)
+    # plt.show()
+    # mpimg.imsave("warped_road_line1.png", warped_road_line)
+    # uuwarped_road1 = unwarp(warped_road_line, src, dst, img_size)
+    # plt.imshow(uuwarped_road1)
+    # plt.show()
+    # mpimg.imsave("uuwarped_road1.png", uuwarped_road1)
+
+    # plot lines on the warped road image:
+    # warped_road_0 = np.zeros_like(warped_road)
+    # fig1 = plt.gcf()
+    # plt.imshow(warped_road_0)
+    # plt.plot(left_fit_cr, ploty, color='yellow')
+    # plt.plot(right_fit_cr, ploty, color='yellow')
+    # plt.title('warped road line', fontsize=10)
+    # fig1.savefig('warped_road_line.png', dpi=100)
+    # mpimg.imsave("warped_road_line.png", warped_road_0)
+    # plt.show()
+    #
+    # uuwarped_road = unwarp(warped_road_0, src, dst, img_size)
+    # plt.imshow(uuwarped_road)
+    # plt.title('unwarped road line', fontsize=10)
+    # mpimg.imsave("uuwarped_road2.png", uuwarped_road)
+    # plt.show()
+
+    # Define conversions in x and y from pixels space to meters
+    ym_per_pix = 30/720 # meters per pixel in y dimension
+    xm_per_pix = 3.7/700 # meters per pixel in x dimension
+
+    # calculate the curve raduis in meters
+    left_curverad, right_curverad = measure_curvature_real(ploty, left_fit_cr, right_fit_cr, ym_per_pix, xm_per_pix)
+    #print(left_curverad, 'm', right_curverad, 'm')
+    #calculate average of curvature raduis
+    R_curve = (left_curverad + right_curverad)/2
+    text_R = '{} meters raduis of curvature'.format(round(R_curve,2))
+
+    lane_mid = (left_fit_cr[0] + right_fit_cr[0])/2
+    middle_img = img_size[0]/2
+    car_off_center = (lane_mid - middle_img)*xm_per_pix
+    #print(car_off_center)
+
+    if car_off_center >= 0:
+        text_C = '{} meters left of center'.format(round(car_off_center,2))
+    else:
+        text_C = '{} meters right of center'.format(round(-car_off_center,2))
+
+    # # font
+    # font = cv2.FONT_HERSHEY_SIMPLEX
+    #
+    # # org
+    # org = (50, 50)
+    #
+    # # fontScale
+    # fontScale = 1
+    #
+    # # Blue color in BGR
+    # color = (255, 0, 0)
+    #
+    # # Line thickness of 2 px
+    # thickness = 2
+
+    # Using cv2.putText() method
+    cv2.putText(undist_road, text_C, (50, 50), cv2.FONT_HERSHEY_SIMPLEX,
+                       1, (255, 0, 0), 2, cv2.LINE_AA)
+    cv2.putText(undist_road, text_R, (50, 100), cv2.FONT_HERSHEY_SIMPLEX,
+                      1, (255, 0, 0), 2, cv2.LINE_AA)
+    # Create a "color" binary image to combine with line image
+    result = cv2.addWeighted(undist_road, 0.5, uuwarped_binary, 1., 0.)
+
+    return result
+
+# Read in the distorted test image
+image_path = '../test_images/straight_lines1.jpg'
+image = cv2.imread(image_path)
+
+#printing out some stats and plotting
+print('This image is:', type(image), 'with dimensions:', image.shape)
+
+result = Lane_Finding_Pipeline_Image_Advanced(image)
+plt.imshow(result)
+plt.show()
+mpimg.imsave("result.png", result)
+
 
 """
-# For fun: get perspective transform of the original road image
-#plt.imshow(warped_image)
-# plt.plot(src[0,0],src[0,1],'.')
-# plt.plot(src[1,0],src[1,1],'.')
-# plt.plot(src[2,0],src[2,1],'.')
-#plt.plot(src[3,0],src[3,1],'.')
-#plt.plot(dst[0,0],dst[0,1],'.')
-#plt.plot(dst[1,0],dst[1,1],'.')
-#plt.plot(dst[2,0],dst[2,1],'.')
-#plt.plot(dst[3,0],dst[3,1],'.')
-#plt.show()
-image_rectangale=cv2.line(image, (src[0,0],src[0,1]), (src[1,0],src[1,1]), (0, 255, 0) , 2)
-image_rectangale=cv2.line(image_rectangale, (src[3,0],src[3,1]), (src[2,0],src[2,1]), (0, 255, 0) , 2)
-image_rectangale=cv2.line(image_rectangale, (src[0,0],src[0,1]), (src[3,0],src[3,1]), (0, 255, 0) , 2)
-image_rectangale=cv2.line(image_rectangale, (src[1,0],src[1,1]), (src[2,0],src[2,1]), (0, 255, 0) , 2)
-mpimg.imsave("image_rectangale.png", image_rectangale)
-warped_image = warp(image_rectangale, src, dst)
-"""
-# Find our lane pixels first
-leftx, lefty, rightx, righty, binary_warped_pixel = find_lane_pixels(binary_warped)
+# Import everything needed to edit/save/watch video clips
+from moviepy.editor import VideoFileClip
+from IPython.display import HTML
 
-# Fit a polynomial
-binary_warped_line = fit_polynomial(binary_warped_pixel, leftx, lefty, rightx, righty)
+video_out = 'project_video_output.mp4'
+# white_output = 'test_videos_output/challenge2.mp4'
+## To speed up the testing process you may want to try your pipeline on a shorter subclip of the video
+## To do so add .subclip(start_second,end_second) to the end of the line below
+## Where start_second and end_second are integer values representing the start and end of the subclip
+## You may also uncomment the following line for a subclip of the first 5 seconds
+##clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
 
-# plt.imshow(binary_warped_pixel)
-# plt.show()
-# mpimg.imsave("binary_warped_pixel.png", binary_warped_pixel)
-
-# plt.imshow(binary_warped_line)
-# plt.show()
-mpimg.imsave("binary_warped_line.png", binary_warped_line)
-
-
-"""
-# Plotting thresholded images
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
-f.tight_layout()
-ax1.set_title('Stacked thresholds')
-ax1.imshow(color_binary)
-
-ax2.set_title('Combined S channel and gradient thresholds')
-ax2.imshow(combined_binary, cmap='gray')
-plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-plt.show()
-mpimg.imsave("combined_binary.png", combined_binary)
-
-#visualize perspective transform on binary image
-f, (ax1,ax2) = plt.subplots(1, 2, figsize=(20,10))
-
-ax1.set_title('Source image')
-ax1.imshow(combined_binary)
-ax2.set_title('warped image')
-ax2.imshow(warped_im)
-plt.show()
-mpimg.imsave("warped.png", binary_warped)
-
-#visualize perspective transform on original road image
-f, (ax1,ax2) = plt.subplots(1, 2, figsize=(20,10))
-
-ax1.set_title('Source image')
-ax1.imshow(image_rectangale)
-ax2.set_title('warped image')
-ax2.imshow(warped_image)
-plt.show()
-mpimg.imsave("warped_road.png", warped_image)
+clip1 = VideoFileClip("../project_video.mp4").subclip(0,2)
+#clip1 = VideoFileClip("test_videos/challenge.mp4")
+clip = clip1.fl_image(Lane_Finding_Pipeline_Image_Advanced) #NOTE: this function expects color images!!
+clip.write_videofile(video_out, audio=False)
 """
